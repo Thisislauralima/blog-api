aa0caa8d79cfa92cf57662a7a1ce2922cd74a396
Laura Lima <lauraolima2000@gmail.com>
2022-06-22 10:17:56 -0300

    feat: create postCategories migration

---
+++ src/database/migrations/20220621210338-create-postCategories.js
@@ -0,0 +1,33 @@
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true
+      },
+      categoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true
+      }
+    }, {
+        timestamps: false,
+        createdAt: false,
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
