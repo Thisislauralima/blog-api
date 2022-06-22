6f13e97bf258234c9f738774978b0fad8251f1fd
Laura Lima <lauraolima2000@gmail.com>
2022-06-22 11:11:06 -0300

    feat: create blogPost model

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,20 @@
+const BlogPostSchema = (sequelize, DataTypes) => {
+  const BlogPostTable = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE
+  }, {
+    timestamps: false,
+  });
+
+  BlogPostTable.associate = (models) => {
+    BlogPostTable.belongsTo(models.User, { as: 'users', foreignKey: 'userId' });
+  }
+
+  return BlogPostTable;
+};
+
+module.exports = BlogPostSchema;
