0b97a81a3005c801cc0a61f3e2f20ca56592b6d6
Laura Lima <lauraolima2000@gmail.com>
2022-06-23 16:55:59 -0300

    feat: add user post route

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ const rescue = require('express-rescue');
 
 const router = express.Router();
 
-const blogController = require('./controllers/blog.controller');
+const loginController = require('./controllers/login.controller');
+const userController = require('./controllers/user.controller');
+
 const middlewares = require('./middlewares');
 
-router.post('/login', middlewares.validateLogin, rescue(blogController.login));
+router.post('/login', middlewares.validateLogin, rescue(loginController.login));
+router.post('/user', middlewares.validateFields, rescue(userController.setUser));
 
 module.exports = router;
