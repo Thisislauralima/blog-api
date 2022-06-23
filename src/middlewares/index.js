2178d66c7b405033053590a044ee68290e3ff927
Laura Lima <lauraolima2000@gmail.com>
2022-06-23 16:59:05 -0300

    feat: add validateFields function to middlewares

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 const { authMiddleware } = require('./authenticateMiddleware');
 const { errorMiddleware } = require('./errorMiddleware');
 const { validateLogin } = require('./loginMiddleware');
+const { validateFields } = require('./userMiddleware');
 
 module.exports = {
   authMiddleware,
   errorMiddleware,
   validateLogin,
+  validateFields,
 };
