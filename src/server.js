fc3c4c814fc8c1ba9c9edddfb50b277cf31e7e1a
Laura Lima <lauraolima2000@gmail.com>
2022-06-23 14:51:07 -0300

    feat: add middlewares to server file

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
+
 const app = require('./api');
+
+const router = require('./routes');
+
+const { errorMiddleware } = require('./middlewares');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
+app.use(router);
 
 // não remova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use(errorMiddleware);
 
 app.listen(port, () => console.log('ouvindo porta', port));
