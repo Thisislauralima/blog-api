9284f0b23490499bfa047dd0f1cd9f1f1f8d7e79
Laura Lima <lauraolima2000@gmail.com>
2022-06-24 18:04:13 -0300

    feat: create post service

---
+++ src/services/post.service.js
@@ -0,0 +1,49 @@
+const { BlogPost, PostCategory, Category } = require('../database/models');
+const { CATEGORY_ID_NOT_FOUND } = require('../utils/constants');
+
+const checkIdsOnDb = async (categoryIds) => {
+  const isIdRegistered = Promise.all(categoryIds.map((el) => 
+    Category.findAll({
+      where: { id: el },
+    })));
+  return isIdRegistered;
+};
+
+const verifyIdLength = async (categoryIds) => {
+  const isIdRegistered = await checkIdsOnDb(categoryIds);
+  return isIdRegistered.map((el) => el.length);
+};
+
+const createdPost = async (title, content, categoryIds, user) => {
+  const post = await BlogPost.create({
+    title,
+    content,
+    userId: user.userId,
+  });
+
+  Promise.all(categoryIds.map((el) =>
+    PostCategory.create({
+      categoryId: el,
+      postId: post.dataValues.id,
+    })));
+
+  return {
+    id: post.dataValues.id,
+    title,
+    content,
+    userId: user.userId,
+    updated: post.dataValues.updated,
+    published: post.dataValues.updated,
+  };
+};
+
+const setPost = async (title, content, categoryIds, user) => {
+  const isIdRegisteredTest = await verifyIdLength(categoryIds);
+  if (isIdRegisteredTest.includes(0)) return CATEGORY_ID_NOT_FOUND;
+
+  return createdPost(title, content, categoryIds, user);
+};
+
+module.exports = {
+  setPost,
+};
