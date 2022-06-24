d95ce7d30e218ac1f5f85160f4e73f6d4c5d87bb
Laura Lima <lauraolima2000@gmail.com>
2022-06-24 11:49:16 -0300

    feat: add some new messages

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 const FIELDS_MISSING = { code: 400, message: 'Some required fields are missing' };
 const INVALID_FIELDS = { code: 400, message: 'Invalid fields' };
-const MALFORMED_TOKEN = { status: 401, message: 'jwt malformed' };
+const TOKEN_NOT_FOUND = { status: 401, message: 'Token not found' };
+const INVALID_TOKEN = { status: 401, message: 'Expired or invalid token' };
 const DISPLAY_NAME_MIN_LENGTH = { code: 400,
   message: '"displayName" length must be at least 8 characters long' };
 const INVALID_EMAIL = { code: 400,
@@ -14,10 +15,11 @@ const EMAIL_REGEX = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
 module.exports = {
   FIELDS_MISSING,
   INVALID_FIELDS,
-  MALFORMED_TOKEN,
+  TOKEN_NOT_FOUND,
   DISPLAY_NAME_MIN_LENGTH,
   EMAIL_REGEX,
   INVALID_EMAIL,
   PASSWORD_MIN_LENGTH,
   USER_ALREADY_REGISTERED,
+  INVALID_TOKEN,
 };
