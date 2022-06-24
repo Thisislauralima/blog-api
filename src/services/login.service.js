56dfbe42c89db1c2cd310f55837845afc435de54
Laura Lima <lauraolima2000@gmail.com>
2022-06-24 17:59:45 -0300

    feat: create login service

---
+++ src/services/login.service.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../database/models');
+const { INVALID_FIELDS } = require('../utils/constants');
+
+const TOKEN_SECRET = process.env.JWT_SECRET;
+
+const jwtConfig = {
+  expiresIn: '1h',
+  algorithm: 'HS256',
+};
+
+const userLogin = async (email, password) => {
+  const user = await User.findAll({ where: { email, password } });
+  if (!user.length) {
+    return INVALID_FIELDS;
+  }
+  const token = jwt.sign({ userId: user[0].dataValues.id, email }, TOKEN_SECRET, jwtConfig);
+  return token;
+};
+
+module.exports = {
+  userLogin,
+};
