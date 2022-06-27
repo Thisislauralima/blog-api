b9314bdc022b8823466d3b30b4bbfec11d4dfd83
Laura Lima <lauraolima2000@gmail.com>
2022-06-27 16:25:47 -0300

    refactor: config file for personal project

--- src/server.js
+++ src/server.js
@@ -6,14 +6,9 @@ const router = require('./routes');
 
 const { errorMiddleware } = require('./middlewares');
 
-// não remova a variável `API_PORT` ou o `listen`
-const port = process.env.API_PORT || 3000;
-app.use(router);
+const port = process.env.API_PORT;
 
-// não remova esse endpoint
-app.get('/', (_request, response) => {
-  response.send();
-});
+app.use(router);
 
 app.use(errorMiddleware);
 
